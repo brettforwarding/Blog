6f2fe8123532e293e2e9a4c4fc26a3e2b666f577
Brett <brettforwarding@gmail.com>
2012-03-23 01:59:32 -0300

    Heroku troubleshooting show code - step 8

--- lib/blog.rb
+++ lib/blog.rb
@@ -36,9 +36,9 @@ module Blog
   end
   
   get '/showcode/:code' do |code|
-    #filecontent = File.open("#{code}.rb", "r") { |f| f.read }
+    filecontents = File.open("lib/#{code}.rb", "r") { |f| f.read }
     #filecontent = File.read("#{code}.rb")
-    filecontents = File.read("lib/test_sort.rb")
+    #filecontents = File.read("lib/test_sort.rb")
     filecontent = "blah"
     erb :showcode, :locals => {:filecontent => filecontent, :code => code}
   end  
